1f2969903f2c571125d34a13f9ea95d0b77fca1e
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-21 12:03:18 -0400

    IPartitionManager has method for getting control for a given partition

--- ICD.Connect.Partitioning/IPartitionManager.cs
+++ ICD.Connect.Partitioning/IPartitionManager.cs
@@ -1,4 +1,6 @@
-﻿using ICD.Connect.Partitioning.Partitions;
+﻿using ICD.Common.Properties;
+using ICD.Connect.Partitioning.Controls;
+using ICD.Connect.Partitioning.Partitions;
 using ICD.Connect.Settings;
 
 namespace ICD.Connect.Partitioning
@@ -6,5 +8,14 @@ namespace ICD.Connect.Partitioning
 	public interface IPartitionManager : IOriginator
 	{
 		IOriginatorCollection<IPartition> Partitions { get; }
+
+		/// <summary>
+		/// Gets the control for the given partition.
+		/// Returns null if the partition has no control specified.
+		/// </summary>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		IPartitionDeviceControl GetControl(IPartition partition);
 	}
 }
