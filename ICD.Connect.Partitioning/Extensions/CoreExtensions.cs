26a857fe81abed327ec83a49ee86d7eeaa7202c4
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-21 11:20:23 -0400

    Initial commit of CoreExtensions

---
+++ ICD.Connect.Partitioning/Extensions/CoreExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Connect.Partitioning.Extensions
+{
+	public static class CoreExtensions
+	{
+		/// <summary>
+		/// Gets the partition manager instance from the core.
+		/// </summary>
+		/// <param name="core"></param>
+		/// <returns></returns>
+		[NotNull]
+		public static IPartitionManager GetPartitionManager(this ICore core)
+		{
+			if (core == null)
+				throw new ArgumentNullException("core");
+
+			return core.Originators.GetChild<IPartitionManager>();
+		}
+	}
+}
