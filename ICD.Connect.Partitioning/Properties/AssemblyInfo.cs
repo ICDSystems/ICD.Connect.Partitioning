b055ecbadf69b698f1bc4aeef77b40fa7165436b
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 10:35:18 -0500

    Updating copyright info

--- ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("ICD.Connect.Partitioning")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
