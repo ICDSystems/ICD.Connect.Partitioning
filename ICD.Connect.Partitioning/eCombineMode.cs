3b1d2d70ec9a75ad5b84108768b68790ac9c2a3d
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-01 11:07:53 -0500

    Initial commit of eCombineMode

---
+++ ICD.Connect.Partitioning/eCombineMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ICD.Connect.Partitioning
+{
+	[Flags]
+	public enum eCombineMode
+	{
+		None = 0,
+		Single = 1,
+		Slave = 2,
+		Master = 4,
+		Combine = Slave | Master,
+		Always = Single | Combine,
+	}
+}
