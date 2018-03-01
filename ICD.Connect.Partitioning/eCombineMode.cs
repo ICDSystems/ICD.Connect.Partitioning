c0a90ec7038a5d063420073cf893e2b633368312
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-01 11:19:16 -0500

    Adding comments

--- ICD.Connect.Partitioning/eCombineMode.cs
+++ ICD.Connect.Partitioning/eCombineMode.cs
@@ -2,14 +2,40 @@
 
 namespace ICD.Connect.Partitioning
 {
+	/// <summary>
+	/// eCombineMode describes the avilability of room contents based on the current combine state.
+	/// </summary>
 	[Flags]
 	public enum eCombineMode
 	{
+		/// <summary>
+		/// Never available.
+		/// </summary>
 		None = 0,
+
+		/// <summary>
+		/// Available in a single room.
+		/// </summary>
 		Single = 1,
+
+		/// <summary>
+		/// Available in a slaved room.
+		/// </summary>
 		Slave = 2,
+
+		/// <summary>
+		/// Available in a master room.
+		/// </summary>
 		Master = 4,
+
+		/// <summary>
+		/// Available in a combined room.
+		/// </summary>
 		Combine = Slave | Master,
+
+		/// <summary>
+		/// Always available.
+		/// </summary>
 		Always = Single | Combine,
 	}
 }
