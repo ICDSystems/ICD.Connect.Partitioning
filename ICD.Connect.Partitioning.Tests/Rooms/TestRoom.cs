f9f2d7382f012a26997012d8ba2b1d33baf4ec9f
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-02 10:52:28 -0500

    Initial commit of TestRoom

---
+++ ICD.Connect.Partitioning.Tests/Rooms/TestRoom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+using ICD.Connect.Partitioning.Rooms;
+using ICD.Connect.Settings.Core;
+using ICD.Connect.Settings.Tests;
+
+namespace ICD.Connect.Partitioning.Tests.Rooms
+{
+	public sealed class TestRoom : AbstractTestOriginator<TestRoomSettings>, IRoom
+	{
+		public event EventHandler<BoolEventArgs> OnCombineStateChanged;
+
+		public ICore Core { get; set; }
+
+		public bool CombineState { get { throw new NotImplementedException(); } }
+
+		public int CombinePriority { get; set; }
+
+		public RoomOriginatorIdCollection Originators { get; set; }
+
+		public void EnterCombineState()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LeaveCombineState()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public sealed class TestRoomSettings: AbstractTestSettings
+	{
+	}
+}
