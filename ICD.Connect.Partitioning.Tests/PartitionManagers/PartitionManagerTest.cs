546a21066b93f51ee6b53ed1a4f068b3832e2c7a
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-16 15:00:03 -0400

    Initial commit of PartitionManagerTest

---
+++ ICD.Connect.Partitioning.Tests/PartitionManagers/PartitionManagerTest.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+
+namespace ICD.Connect.Partitioning.Tests.PartitionManagers
+{
+	[TestFixture]
+	public sealed class PartitionManagerTest
+	{
+		[Test]
+		public void PartitionOpenStateChangeFeedbackTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		#region Controls
+
+		[Test]
+		public void GetControlsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		#endregion
+
+		#region Combine
+
+		[Test]
+		public void CombinesRoomPartitionTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void CombinesRoomPartitionIdTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetCombineRoomTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetTopLevelRoomsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void ToggleCombineRoomsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void InitializeCombineRoomsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void CombineRoomsPartitionsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void CombineRoomsControlsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void CombineRoomsPartitionTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void CombineRoomsPartitionControlTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void UncombineRoomsPartitionsTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void UncombineRoomsPartitionTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void UncombineRoomsPartitionControlTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		#endregion
+	}
+}
