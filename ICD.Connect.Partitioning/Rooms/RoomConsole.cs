e45f4de4a696fadf7d45a06242635d863ab9554d
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-25 12:06:22 -0400

    feat: Adding console command for profiling random routing operations

--- ICD.Connect.Partitioning/Rooms/RoomConsole.cs
+++ ICD.Connect.Partitioning/Rooms/RoomConsole.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ICD.Common.Utils;
+using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Timers;
 using ICD.Connect.API.Commands;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Devices;
 using ICD.Connect.Panels;
 using ICD.Connect.Protocol.Ports;
+using ICD.Connect.Routing;
+using ICD.Connect.Routing.Connections;
+using ICD.Connect.Routing.Controls;
 
 namespace ICD.Connect.Partitioning.Rooms
 {
@@ -55,9 +61,37 @@ namespace ICD.Connect.Partitioning.Rooms
 			if (instance == null)
 				throw new ArgumentNullException("instance");
 
+#if DEBUG
 			yield return
 				new GenericConsoleCommand<int>("SetCombinePriority", "SetCombinePriority <PRIORITY>",
 				                               i => instance.CombinePriority = i);
+
+			yield return new GenericConsoleCommand<int>("ProfileRouting", "ProfileRouting <ITERATIONS>", i => ProfileRouting(instance, i));
+#endif
+		}
+
+		private static void ProfileRouting(IRoom instance, int iterations)
+		{
+			IRouteSwitcherControl[] switchers =
+				instance.Originators
+				        .GetInstancesRecursive<IDeviceBase>()
+				        .Select(d => d.Controls.GetControl<IRouteSwitcherControl>())
+				        .Where(c => c != null)
+				        .Distinct()
+				        .ToArray();
+
+			IcdStopwatch.Profile(() => RouteRandom(switchers), iterations, "Profile Routing");
+		}
+
+		private static void RouteRandom(IEnumerable<IRouteSwitcherControl> switchers)
+		{
+			IRouteSwitcherControl switcher = switchers.Random();
+			ConnectorInfo input = switcher.GetInputs().Random();
+			ConnectorInfo output = switcher.GetOutputs().Random();
+
+			eConnectionType type = EnumUtils.GetFlagsIntersection(input.ConnectionType, output.ConnectionType);
+
+			switcher.Route(input.Address, output.Address, type);
 		}
 	}
 }
