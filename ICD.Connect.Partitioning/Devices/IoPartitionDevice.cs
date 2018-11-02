4b76c18361523203f10aaa9401c19bb86e108f3f
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-02 14:05:41 -0400

    refactor: Tidying

--- ICD.Connect.Partitioning/Devices/IoPartitionDevice.cs
+++ ICD.Connect.Partitioning/Devices/IoPartitionDevice.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ICD.Common.Properties;
 using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Services.Logging;
