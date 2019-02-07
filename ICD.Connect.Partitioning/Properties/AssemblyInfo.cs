f195b5bda61199a03a122ff928819a37bb19262f
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:32:57 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Partitioning")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("10.0.1.0")]
