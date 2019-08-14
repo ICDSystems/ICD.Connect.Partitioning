0a36cdda546cb393bc60ac14826b7be6ff87ff78
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-08-14 14:49:01 -0400

    feat: create abstract commercial room

---
+++ ICD.Connect.Partitioning.Commercial/ICommercialRoom.cs
@@ -0,0 +1,17 @@
+﻿using ICD.Connect.Partitioning.Rooms;
+
+namespace ICD.Connect.Partitioning.Commercial
+{
+	public interface ICommercialRoom : IRoom
+	{
+		/// <summary>
+		/// Gets the wake/sleep schedule.
+		/// </summary>
+		WakeSchedule WakeSchedule { get; }
+
+		/// <summary>
+		/// Gets the path to the loaded dialing plan xml file. Used by fusion :(
+		/// </summary>
+		DialingPlanInfo DialingPlan { get; }
+	}
+}
---
+++ ICD.Connect.Partitioning.Commercial/ICommercialRoomSettings.cs
@@ -0,0 +1,14 @@
+﻿using ICD.Connect.Partitioning.Rooms;
+
+namespace ICD.Connect.Partitioning.Commercial
+{
+	public interface ICommercialRoomSettings : IRoomSettings
+	{
+		 WakeSchedule WakeSchedule { get; }
+
+		 /// <summary>
+		 /// Gets the dialing plan.
+		 /// </summary>
+		 DialingPlanInfo DialingPlan { get; set; }
+	}
+}
