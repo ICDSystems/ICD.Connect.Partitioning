4d45f3d9862bbbe1749c4dde759c23914c0c4034
Drew Tingen <drew.tingen@icdsystems.com>
2020-04-06 18:14:46 -0400

    feat: MockPartitionDevice implements IMockDevice

--- ICD.Connect.Partitioning/Devices/MockPartitionDevice.cs
+++ ICD.Connect.Partitioning/Devices/MockPartitionDevice.cs
@@ -1,10 +1,18 @@
-﻿using ICD.Common.Properties;
+﻿using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices.Mock;
 using ICD.Connect.Partitioning.Controls;
+using ICD.Connect.Settings;
 
 namespace ICD.Connect.Partitioning.Devices
 {
-	public sealed class MockPartitionDevice : AbstractPartitionDevice<MockPartitionDeviceSettings>
+	public sealed class MockPartitionDevice : AbstractPartitionDevice<MockPartitionDeviceSettings>, IMockDevice
 	{
+
+		private bool m_IsOnline;
+
 		/// <summary>
 		/// Returns the mask for the type of feedback that is supported,
 		/// I.e. if we can set the open state of the partition, and if the partition
@@ -12,7 +20,24 @@ namespace ICD.Connect.Partitioning.Devices
 		/// </summary>
 		public override ePartitionFeedback SupportsFeedback { get { return ePartitionFeedback.GetSet; } }
 
+		public bool DefaultOffline { get; set; }
+
 		#region Methods
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			return m_IsOnline;
+		}
+
+		public void SetIsOnlineState(bool isOnline)
+		{
+			m_IsOnline = isOnline;
+			UpdateCachedOnlineStatus();
+		}
 
 		[PublicAPI]
 		public override void Open()
@@ -28,13 +53,74 @@ namespace ICD.Connect.Partitioning.Devices
 
 		#endregion
 
+		#region Settings
+
 		/// <summary>
-		/// Gets the current online status of the device.
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(MockPartitionDeviceSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			MockDeviceHelper.ApplySettings(this, settings);
+		}
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(MockPartitionDeviceSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			MockDeviceHelper.CopySettings(this, settings);
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			MockDeviceHelper.ClearSettings(this);
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			MockDeviceHelper.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
 		/// </summary>
 		/// <returns></returns>
-		protected override bool GetIsOnlineStatus()
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
 		{
-			return true;
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in MockDeviceHelper.GetConsoleCommands(this))
+				yield return command;
 		}
+
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
 	}
 }
--- ICD.Connect.Partitioning/Devices/MockPartitionDeviceSettings.cs
+++ ICD.Connect.Partitioning/Devices/MockPartitionDeviceSettings.cs
@@ -1,9 +1,34 @@
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices.Mock;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Partitioning.Devices
 {
 	[KrangSettings("MockPartition", typeof(MockPartitionDevice))]
-	public sealed class MockPartitionDeviceSettings : AbstractPartitionDeviceSettings
+	public sealed class MockPartitionDeviceSettings : AbstractPartitionDeviceSettings, IMockDeviceSettings
 	{
+		public bool DefaultOffline { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			MockDeviceSettingsHelper.WriteElements(this, writer);
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			MockDeviceSettingsHelper.ParseXml(this, xml);
+		}
 	}
 }
