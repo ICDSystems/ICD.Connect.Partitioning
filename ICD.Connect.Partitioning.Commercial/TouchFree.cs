4b284f40a46b08421122ca3306a0e43a81981ba2
Laura Gomez <laura.gomez@profound-tech.com>
2020-06-23 15:24:02 -0400

    feat : Implemented Settings for the Touch Free Settings page.

---
+++ ICD.Connect.Partitioning.Commercial/TouchFree.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.Routing.Endpoints.Sources;
+using ICD.Connect.Routing.EventArguments;
+using ICD.Connect.Settings;
+
+namespace ICD.Connect.Partitioning.Commercial
+{
+	public sealed class TouchFree
+	{
+		public event EventHandler<IntEventArgs> OnCountDownTimerChanged;
+		public event EventHandler<BoolEventArgs> OnEnabledChanged;
+		public event EventHandler<SourceEventArgs> OnDefaultSourceChanged; 
+
+		private int m_CountDownSeconds ;
+		private bool m_Enabled;
+		private ISource m_Source;
+
+		#region Properties
+
+		public int CountdownSeconds
+		{
+			get {return m_CountDownSeconds;}
+			set
+			{
+				if (value == m_CountDownSeconds)
+					return;
+
+				m_CountDownSeconds = value;
+
+				OnCountDownTimerChanged.Raise(this, new IntEventArgs(m_CountDownSeconds));
+			}
+		}
+
+		public bool Enabled
+		{
+			get {return m_Enabled;}
+			set
+			{
+				if (value == m_Enabled)
+					return;
+
+				m_Enabled = value;
+
+				OnEnabledChanged.Raise(this, new BoolEventArgs(m_Enabled));
+			}
+		}
+
+		[CanBeNull]
+		public ISource Source
+		{
+			get { return m_Source; }
+			set
+			{
+				if (value == m_Source)
+					return;
+
+				m_Source = value;
+
+				OnDefaultSourceChanged.Raise(this, new SourceEventArgs(m_Source));
+			}
+		}
+
+		#endregion
+
+		#region Settings
+
+		public void ClearSettings()
+		{
+			CountdownSeconds = 0;
+			Enabled = false;
+			Source = null;
+		}
+
+		/// <summary>
+		/// Copies the properties onto the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		public void CopySettings(TouchFreeSettings settings)
+		{
+			if (settings == null)
+				throw new ArgumentNullException("settings");
+
+			settings.CountdownSeconds = CountdownSeconds;
+			settings.Enabled = Enabled;
+
+			settings.SourceId = Source == null ? (int?)null : Source.Id;
+		}
+
+		public void ApplySettings(TouchFreeSettings settings, IDeviceFactory factory)
+		{
+			CountdownSeconds = settings.CountdownSeconds;
+			Enabled = settings.Enabled;
+
+			try
+			{
+				Source = settings.SourceId == null ? null : factory.GetOriginatorById<ISource>(settings.SourceId.Value);
+			}
+			catch (KeyNotFoundException)
+			{
+				ServiceProvider.GetService<ILoggerService>().AddEntry(eSeverity.Error, "{0} - No Source with ID {1}", GetType().Name, settings.SourceId);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Partitioning.Commercial/TouchFreeSettings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ICD.Common.Utils.Xml;
+
+namespace ICD.Connect.Partitioning.Commercial
+{
+	public sealed class TouchFreeSettings
+	{
+		private const string COUNTDOWN_SECONDS_ELEMENT = "Countdown";
+		private const string ENABLED_ELEMENT = "Enabled";
+		private const string SOURCE_ELEMENT = "Source";
+
+
+		public int CountdownSeconds { get; set; }
+
+		public bool Enabled { get; set; }
+
+		public int? SourceId { get; set; }
+
+		/// <summary>
+		/// Copies the properties from the other instance.
+		/// </summary>
+		/// <param name="other"></param>
+		public void Copy(TouchFreeSettings other)
+		{
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			CountdownSeconds = other.CountdownSeconds;
+			Enabled = other.Enabled;
+			SourceId = other.SourceId;
+		}
+
+		public void Clear()
+		{
+			CountdownSeconds = 0;
+			Enabled = false;
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public void ParseXml(string xml)
+		{
+			CountdownSeconds = XmlUtils.TryReadChildElementContentAsInt(xml, COUNTDOWN_SECONDS_ELEMENT) ?? 0;
+			Enabled = XmlUtils.TryReadChildElementContentAsBoolean(xml, ENABLED_ELEMENT) ?? false;
+			SourceId = XmlUtils.TryReadChildElementContentAsInt(xml, SOURCE_ELEMENT);
+		}
+
+		/// <summary>
+		/// Writes the settings to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="element"></param>
+		public void WriteElements(IcdXmlTextWriter writer, string element)
+		{
+			writer.WriteStartElement(element);
+			{
+				writer.WriteElementString(COUNTDOWN_SECONDS_ELEMENT, IcdXmlConvert.ToString(CountdownSeconds));
+
+				writer.WriteElementString(ENABLED_ELEMENT, IcdXmlConvert.ToString(ENABLED_ELEMENT));
+
+				writer.WriteElementString(SOURCE_ELEMENT, IcdXmlConvert.ToString(SOURCE_ELEMENT));
+			}
+			writer.WriteEndElement();
+		}
+	}
+}
