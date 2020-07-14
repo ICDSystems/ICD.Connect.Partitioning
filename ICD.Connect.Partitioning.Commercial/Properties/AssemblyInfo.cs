d10403d21d750abee0f1e7704122323dd5941ffa
Chris Cameron <chris.cameron@wedoresi.com>
2020-07-14 14:35:41 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Partitioning.Commercial/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning.Commercial/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning.Commercial")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("15.0.0.0")]
+[assembly: AssemblyVersion("15.1.0.0")]
