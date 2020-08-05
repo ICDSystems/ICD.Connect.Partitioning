3419c7726a3f8d958f898132e2815d4e1fc0c005
Chris Cameron <chris.cameron@wedoresi.com>
2020-08-05 10:58:24 -0400

    feat: Added room mute telemetry

--- ICD.Connect.Partitioning.Commercial/Controls/Occupancy/OccupancyTelemetryNames.cs
+++ ICD.Connect.Partitioning.Commercial/Controls/Occupancy/OccupancyTelemetryNames.cs
@@ -1,6 +1,6 @@
 ﻿namespace ICD.Connect.Partitioning.Commercial.Controls.Occupancy
 {
-	public class OccupancyTelemetryNames
+	public static class OccupancyTelemetryNames
 	{
 		public const string OCCUPANCY_STATE = "Occupancy State";
 		public const string OCCUPANCY_STATE_CHANGED = "Occupancy State Changed";
---
+++ ICD.Connect.Partitioning/Rooms/RoomTelemetryNames.cs
@@ -0,0 +1,14 @@
+﻿namespace ICD.Connect.Partitioning.Rooms
+{
+	public static class RoomTelemetryNames
+	{
+		public const string MUTED_CHANGED = "MutedChanged";
+		public const string MUTED = "Muted";
+
+		public const string ORIGINATOR_IDS_CHANGED = "OnOriginatorIdsChanged";
+		public const string ORIGINATOR_IDS = "OriginatorIds";
+
+		public const string VOLUME_PERCENT_CHANGED = "VolumePercentChanged";
+		public const string VOLUME_PERCENT = "VolumePercent";
+	}
+}
