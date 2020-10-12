f6c895af10a4e7b38c80935a4ec178e7802d699e
Chris Cameron <chris.cameron@wedoresi.com>
2020-10-12 13:06:55 -0400

    refactor: Moved Occupancy sensor device and control from Misc project

---
+++ ICD.Connect.Partitioning.Commercial/Controls/Occupancy/OccupancySensorControl.cs
@@ -0,0 +1,24 @@
+﻿using ICD.Common.Utils.EventArguments;
+using ICD.Connect.Partitioning.Commercial.Devices.Occupancy;
+
+namespace ICD.Connect.Partitioning.Commercial.Controls.Occupancy
+{
+	public sealed class OccupancySensorControl : AbstractOccupancySensorControl<IOccupancySensorDevice>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		public OccupancySensorControl(IOccupancySensorDevice parent, int id)
+			: base(parent, id)
+		{
+			parent.OnOccupancyStateChanged += ParentOnOccupancyStateChanged;
+		}
+
+		private void ParentOnOccupancyStateChanged(object sender, GenericEventArgs<eOccupancyState> args)
+		{
+			OccupancyState = args.Data;
+		}
+	}
+}
---
+++ ICD.Connect.Partitioning.Commercial/Devices/Occupancy/IOccupancySensorDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+using ICD.Connect.Devices;
+using ICD.Connect.Partitioning.Commercial.Controls.Occupancy;
+
+namespace ICD.Connect.Partitioning.Commercial.Devices.Occupancy
+{
+	public interface IOccupancySensorDevice: IDevice
+	{
+		event EventHandler<GenericEventArgs<eOccupancyState>> OnOccupancyStateChanged;
+
+		eOccupancyState OccupancyState { get; }
+	}
+}
