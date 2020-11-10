e14a8b793a645e8999c7eeec570f61bb12a8e018
Drew Tingen <drew.tingen@icdsystems.com>
2020-11-10 09:20:05 -0500

    feat: Shim provides partition state feedback

--- ICD.Connect.Partitioning.CrestronSPlus/Shims/SPlusPartitionSensorShim.cs
+++ ICD.Connect.Partitioning.CrestronSPlus/Shims/SPlusPartitionSensorShim.cs
@@ -1,4 +1,7 @@
-﻿using ICD.Common.Properties;
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
 using ICD.Connect.Devices.CrestronSPlus.SPlusShims;
 using ICD.Connect.Partitioning.CrestronSPlus.Devices;
 
@@ -10,6 +13,15 @@ namespace ICD.Connect.Partitioning.CrestronSPlus.Shims
 		#region SPlus
 
 		[PublicAPI("S+")]
+		public event EventHandler OnOpenStatusChanged;
+
+		[PublicAPI("S+")]
+		public ushort IsOpen
+		{
+			get { return (ushort)(Originator != null && Originator.IsOpen ? 1 : 0); }
+		}
+		
+		[PublicAPI("S+")]
 		public void SetPartitionState(ushort state)
 		{
 			if (Originator != null)
@@ -18,5 +30,52 @@ namespace ICD.Connect.Partitioning.CrestronSPlus.Shims
 
 		#endregion
 
+		#region Originator
+
+		/// <summary>
+		/// Subscribes to the originator events.
+		/// </summary>
+		/// <param name="originator"></param>
+		protected override void Subscribe(SPlusPartitionSensorDevice originator)
+		{
+			base.Subscribe(originator);
+
+			if (originator == null)
+				return;
+
+			originator.OnOpenStatusChanged += OriginatorOnOpenStatusChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribes from the originator events.
+		/// </summary>
+		/// <param name="originator"></param>
+		protected override void Unsubscribe(SPlusPartitionSensorDevice originator)
+		{
+			base.Unsubscribe(originator);
+
+			if (originator == null)
+				return;
+
+			originator.OnOpenStatusChanged -= OriginatorOnOpenStatusChanged;
+		}
+
+		private void OriginatorOnOpenStatusChanged(object sender, BoolEventArgs args)
+		{
+			OnOpenStatusChanged.Raise(this);
+		}
+
+		/// <summary>
+		/// Called when the originator is attached.
+		/// Do any actions needed to syncronize
+		/// </summary>
+		protected override void InitializeOriginator()
+		{
+			base.InitializeOriginator();
+
+			OnOpenStatusChanged.Raise(this);
+		}
+
+		#endregion
 	}
 }
