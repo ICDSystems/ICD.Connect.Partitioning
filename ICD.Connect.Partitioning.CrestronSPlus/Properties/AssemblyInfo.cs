9853cc205fea5a7c9551ba95a7ccaa929f9ca5df
Chris Cameron <chris.cameron@wedoresi.com>
2021-01-06 16:45:05 -0500

    chore: Updating copyright year

--- ICD.Connect.Partitioning.CrestronSPlus/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning.CrestronSPlus/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("ICD.Connect.Partitioning.CrestronSPlus")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning.CrestronSPlus")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("15.1.0.0")]
 
--- ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Partitioning")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("15.3.0.0")]
