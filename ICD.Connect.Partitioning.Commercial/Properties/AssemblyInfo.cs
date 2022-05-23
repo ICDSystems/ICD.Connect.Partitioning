f3accc65abde8362d4977431b7635693258b49aa
Drew Tingen <drew.tingen@icdsystems.com>
2022-05-23 12:28:04 -0400

    chore: update changelog, increment assembly patch version

--- ICD.Connect.Partitioning.Commercial/Properties/AssemblyInfo.cs
+++ ICD.Connect.Partitioning.Commercial/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Partitioning.Commercial")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
-[assembly: AssemblyVersion("15.7.0.0")]
+[assembly: AssemblyVersion("15.7.1.0")]
